6055571f46285ba85aad4e8a2bfc5bb6a327aace
tiagoaraujo00 <tiagodevjs@gmail.com>
2024-01-19 18:59:02 -0300

    feat: Req 9 refactor

--- app/src/main/java/com/betrybe/trybnb/ui/views/fragments/ReservationFragment.kt
+++ app/src/main/java/com/betrybe/trybnb/ui/views/fragments/ReservationFragment.kt
@@ -5,9 +5,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.betrybe.trybnb.R
 
 class ReservationFragment : Fragment() {
+
+    private lateinit var  mRecyclerView: RecyclerView
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -18,5 +22,11 @@ class ReservationFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        mRecyclerView = view.findViewById(R.id.reservation_recycler_view)
+        mRecyclerView.layoutManager = LinearLayoutManager(context)
+    }
+
+    override fun onStart() {
+        super.onStart()
     }
 }
