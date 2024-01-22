cd8eca0b32adefd19c3fd9d7457e83e08a774f17
tiagoaraujo00 <tiagodevjs@gmail.com>
2024-01-22 18:13:45 -0300

    feat: Req 08

--- app/src/main/java/com/betrybe/trybnb/ui/views/fragments/ReservationFragment.kt
+++ app/src/main/java/com/betrybe/trybnb/ui/views/fragments/ReservationFragment.kt
@@ -8,6 +8,7 @@ import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.betrybe.trybnb.R
+import com.betrybe.trybnb.common.ApiIdlingResource
 import com.betrybe.trybnb.data.api.BookingServiceApi
 import com.betrybe.trybnb.data.models.Bookingids
 import com.betrybe.trybnb.ui.adapters.ListItemAdapter
@@ -29,6 +30,7 @@ class ReservationFragment : Fragment() {
         val view = inflater.inflate(R.layout.fragment_reservation, container, false)
 
         CoroutineScope(Dispatchers.IO).launch {
+            ApiIdlingResource.increment()
             val bookingResponse = apiService.getBooking()
             val bookingIds = bookingResponse.body()?.sortedBy { it.bookingid }?.take("3".toInt())
             val bookingDetailsList = mutableListOf<Bookingids>()
@@ -50,6 +52,7 @@ class ReservationFragment : Fragment() {
                 mRecyclerView.adapter = listItemAdapter
                 listItemAdapter.notifyDataSetChanged()
             }
+            ApiIdlingResource.decrement()
         }
         return view
     }
